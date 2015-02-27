275978a8c4dda68db3243f25036ac2f12c2e54d4
Vic Lau <xingyulliiuu@gmail.com>
2015-02-27 10:02:32 +0800

    Improve.

--- src/chapter21_implicit/implicit_conversion/Fraction.scala
+++ src/chapter21_implicit/implicit_conversion/Fraction.scala
@@ -14,16 +14,14 @@ object Fraction {
   //   smaller[T](a: T, b: T)(implicit order: T => Ordered[T])
   // works for Fraction?
   // Provide a function whose signature is: T => Ordered[T]
-  implicit val fraction2OrderedFraction = {
+  implicit def fraction2OrderedFraction(f: Fraction): Ordered[Fraction] = {
     @tailrec def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m % n)
-    
-    def toOrdered(f: Fraction): Ordered[Fraction] = new Ordered[Fraction] {
+    new Ordered[Fraction] {
       override def compare(that: Fraction): Int = {
         val lcm = f.m * that.m / gcd(f.m, that.m)
         f.n * (lcm / f.m) - that.n * (lcm / that.m)
       }
     }
-    toOrdered _
   }
   
 }
